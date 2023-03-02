55af263fb734b671a8b1e3cb72bb925a43c7d715
Alexey Pozin <pozalek@yandex.ru>
2023-03-02 20:04:16 +0500

    update games/even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,5 +1,5 @@
 import readlineSync from 'readline-sync';
-import getRandomNum from '../getRandomNum.js';
+// import getRandomNum from '../getRandomNum.js';
 
 console.log('Welcome to the Brain Games!');
 const userName = readlineSync.question('May I have your name? ');
@@ -8,14 +8,13 @@ console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
 const parityGame = () => {
   for (let i = 0; i < 3; i += 1) {
-    const randomNumber = getRandomNum(100);
-    const evenNumber = randomNumber % 2 === 0;
-    const oddNumber = randomNumber % 2 !== 0;
+    const randomNumber = Math.ceil(Math.random() * 100);
+    const evenNumber = (randomNumber % 2) === 0;
+    const oddNumber = (randomNumber % 2) !== 0;
     console.log(`Question: ${randomNumber}`);
 
     const userAnswer = readlineSync.question('Your answer: ');
     const correctAnsw = (evenNumber && userAnswer === 'yes') || (oddNumber && userAnswer === 'no');
-    // const wrongAnsw = (evenNumber && userAnswer !== 'yes') || (oddNumber && userAnswer !== 'no');
 
     if (correctAnsw) {
       console.log('Correct!');
