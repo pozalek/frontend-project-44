39a17e74b3976575db5fa56d303a7ae3276bbf64
Alexey Pozin <pozalek@yandex.ru>
2023-03-03 19:33:41 +0500

    added bin/brain-calc.js, src/games/calc.js. Update files

---
+++ src/games/calc.js
@@ -0,0 +1,30 @@
+import readlineSync from 'readline-sync';
+import getRandomNum from '../getRandomNum.js';
+
+const evenNumber = (number) => (number % 2) === 0;
+
+console.log('Welcome to the Brain Games!');
+const userName = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${userName}!`);
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+const brainCalc = () => {
+  for (let i = 0; i < 3; i += 1) {
+    const randomNumber = getRandomNum(100);
+    console.log(`Question: ${randomNumber}`);
+
+    const userAnswer = readlineSync.question('Your answer: ');
+    const correctAnsw = evenNumber(randomNumber) ? 'yes' : 'no';
+    const isAnswCorrect = userAnswer === correctAnsw;
+
+    if (isAnswCorrect) {
+      console.log('Correct!');
+    } else {
+      return console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnsw}'. \nLet's try again, ${userName}!`);
+    }
+  }
+
+  return console.log(`Congratulations, ${userName}!`);
+};
+
+export default brainCalc;
